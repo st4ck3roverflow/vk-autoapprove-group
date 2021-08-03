1600a58b76eb45818af889dd6eac4a8a63e7d6e9
Artur <39272116+arturyudin@users.noreply.github.com>
2021-08-03 15:34:14 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+vk_credentials = ["+79551811382",
+                  "gdu484j@-fshfs",
+                  "992c58ffd62a43e37f47f6d4h5341eaaa59853c567d755339f452953107749ee63b386d1291124041e9234309"
+                  ]  # login, password, token
+vk_group_id = 206251018
